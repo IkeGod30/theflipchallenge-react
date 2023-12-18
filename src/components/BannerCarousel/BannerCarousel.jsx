f220d494233b33b259a3580f12a370d75f560753
Tony Izuogu <anthoizu@yahoo.com>
2023-12-17 20:46:52 -0600

    Added a third carousel item content

--- src/components/BannerCarousel/BannerCarousel.jsx
+++ src/components/BannerCarousel/BannerCarousel.jsx
@@ -69,14 +69,18 @@ function BannCarousel() {
               </div> */}
             </div>
             <div className="carousel-item">
-              <CarouselText />
+              <CarouselText
+                carouselHeading={"Episode 3 Winner"}
+                carouselFeedback={"This is fantastic"}
+                carouselName={"- Maria Nebo"}
+              />
               {/* <img src={} className="d-block w-100" alt={}> */}
-              <div className="carousel-caption d-none d-md-block">
+              {/* <div className="carousel-caption d-none d-md-block">
                 <h5>Third slide label</h5>
                 <p>
                   Some representative placeholder content for the third slide.
                 </p>
-              </div>
+              </div> */}
             </div>
           </div>
           <button
