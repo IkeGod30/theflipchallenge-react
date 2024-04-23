3c510e8317d5fcd5dbb738aa847a53436378833e
Tony Izuogu <anthoizu@yahoo.com>
2024-04-22 21:04:12 -0500

    Deleted commented code in Quiz.jsx

--- src/pages/QuizForm/Quiz.jsx
+++ src/pages/QuizForm/Quiz.jsx
@@ -1,26 +1,11 @@
-function Frame({ resource, quizSrc, Url, frameSrc }) {
+function Frame({ resource }) {
   return (
     <>
       <h2>Welcome to the Quiz Page</h2>
-      {/* <div>
-        <h4>Home</h4>
-        <Link to="/">Home</Link>
-      </div>
-      <div>
-        <h4>Home</h4>
-        <Link to="/how">How To Win</Link>
-      </div>
-      <div>
-        <h4>Contact Us</h4>
-        <Link to="/contact">Contact Us</Link>
-      </div> */}
-
       <div>
         <iframe
           title="Welcome to The Challenge"
           src={resource}
-          // src={resource}
-          // src={quizSrc}
           width="60%"
           height="250px"
         ></iframe>
