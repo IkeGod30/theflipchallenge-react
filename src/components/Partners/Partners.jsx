51fdb29a8cf67a484d8e67a6288a265d5bacc5b9
Tony Izuogu <anthoizu@yahoo.com>
2024-06-21 19:50:14 -0500

    Added and imported images for Sponsors in the PartnerBanner component

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,11 +1,22 @@
-import './Partners.css';
+import "./Partners.css";
+
+import one from "../assets/libry.png";
+import two from "../assets/walmart_.png";
+import three from "../assets/target.png";
 
 function PartnerBanner() {
-    return(
-        <div>
-            <h1>Our Partners</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Our Partners</h1>
+      <div>
+        <marquee behavior="scroll" direction="left">
+          <img src={one} />
+          <img src={two} />
+          <img src={three} />
+        </marquee>
+      </div>
+    </div>
+  );
 }
 
 export default PartnerBanner;
