083e591cdeeeb5257e78f1afb0c6576be485eacb
Tony Izuogu <anthoizu@yahoo.com>
2024-06-23 19:42:10 -0500

    Updated Nav to make it mobile responsive

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,54 +8,73 @@ import { CiCircleChevRight } from "react-icons/ci";
 function Nav() {
   return (
     <>
-      {/* <nav>
-        <Link to="/">Home</Link>
-        <Link to="/how">How</Link>
-        <Link to="/contact">Contact</Link>
-      </nav> */}
-      <header className="priNav">
+      <section className="priNav">
+        <nav
+          className="navbar navbar-expand-lg navbar-dark fixed-top flexContainer"
+          id="mainNav"
+        >
+          <div className="container-fluid">
+            <a className="brand" href="/">
+              {" "}
+              The Flip Challenge
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              Menu
+              <i className="fas fa-bars ms-1"></i>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link" href="/how">
+                    How to Win
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/contact">
+                    Contact Us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/login">
+                    Login
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </section>
+
+      {/* <header className="priNav">
         <nav className="flexContainer">
           <a className="brand" href="/">
             The Flip Challenge
           </a>
-          {/* <Link className="brand" to="/">
-            The Flip Challenge
-          </Link> */}
           <ul className="main-nav-ul">
             <li className="nav-li">
               <a href="/how">How to Win</a>
-              {/* <Link to="/how">
-                <CiCircleChevRight
-                  style={{ color: "white", fontSize: "50px" }}
-                />
-                How to Win
-              </Link> */}
+              <CiCircleChevRight style={{ color: "white", fontSize: "50px" }} />
+              How to Win
             </li>
             <li className="nav-li">
               <a href="/contact">Contact Us</a>
-              {/* <Link to="/contact">
-                <CiCircleChevRight
-                  style={{ color: "white", fontSize: "50px" }}
-                />
-                Contact Us
-              </Link> */}
+              <CiCircleChevRight style={{ color: "white", fontSize: "50px" }} />
+              Contact Us
             </li>
             <li className="nav-li">
               <a href="/login">Login</a>
             </li>
           </ul>
         </nav>
-        {/* <BrowserRouter>
-          <Routes>
-            <Route path="quiz" element={<ShowFrame />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="faq" element={<Faq />} />
-          </Routes>
-        </BrowserRouter> */}
-        {/* 
-        <Outlet /> */}
-      </header>
-      {/* <Outlet /> */}
+      </header> */}
     </>
   );
 }
