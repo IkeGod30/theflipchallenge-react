07a017c3675e658c8bcd45c6fd209e548b897ed3
Tony Izuogu <anthoizu@yahoo.com>
2024-07-06 21:10:49 -0500

    Updated the landing page

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,8 +1,22 @@
+import { Button } from "@mui/material";
 import LandingCarousel from "./LandingCarousel/LandingCarousel";
+import { useNavigate } from "react-router-dom";
+// import Login from "../Login/Login";
+
 export function LandingPage() {
+  const navigate = useNavigate();
+  function challenge() {
+    navigate("/login");
+  }
   return (
-    <div>
-      {/* <h2>Landing Page</h2> */}
+    <div style={{ textAlign: "center" }}>
+      <button
+        onClick={challenge}
+        // style={{ textAlign: "center", margin: "auto" }}
+        className="btn btn-info"
+      >
+        Click to Challenge
+      </button>
       <LandingCarousel />
     </div>
   );
