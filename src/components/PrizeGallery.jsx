c49b86a896c7652c47c6a87d7cb92f86e4c75593
Tony Izuogu <anthoizu@yahoo.com>
2025-03-11 21:50:48 -0500

    Added dev comment

--- src/components/PrizeGallery.jsx
+++ src/components/PrizeGallery.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useReducer } from "react"; // REMOVE
+import { useReducer } from "react"; // REMOVE BEFORE BUILD
 
 import { HashLink as Linkme } from "react-router-hash-link";
 
