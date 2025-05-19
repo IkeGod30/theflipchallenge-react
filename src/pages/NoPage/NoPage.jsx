36e1900033ef4b254cd900014968e9d31fb965b9
Tony Izuogu <anthoizu@yahoo.com>
2025-05-18 19:21:09 -0500

    Edited NoPage route

--- src/pages/NoPage/NoPage.jsx
+++ src/pages/NoPage/NoPage.jsx
@@ -2,8 +2,8 @@ function NoPage() {
   return (
     <div>
       <h2>
-        Unfortunately, the requested page does not exist. Please check and try
-        again. Thank you.
+        Oops! Unfortunately, the requested page does not exist. Please check and
+        try again. Thank you.
       </h2>
     </div>
   );
