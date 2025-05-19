e7e842e443540e00e8639d052fe8de20081cff11
Tony Izuogu <anthoizu@yahoo.com>
2025-05-18 19:28:36 -0500

    Updated NoPage

--- src/pages/NoPage/NoPage.jsx
+++ src/pages/NoPage/NoPage.jsx
@@ -1,7 +1,9 @@
+import "./NoPage.css";
+
 function NoPage() {
   return (
     <div>
-      <h2>
+      <h2 className="head">
         Oops! Unfortunately, the requested page does not exist. Please check and
         try again. Thank you.
       </h2>
