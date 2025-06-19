8ba728c41e8ba4e2b656a5669a555ddec93dc993
Tony Izuogu <anthoizu@yahoo.com>
2025-06-18 21:49:49 -0500

    Edited Quizload.jsx

--- src/components/Quizload.jsx
+++ src/components/Quizload.jsx
@@ -1,9 +1,7 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from 'react';
-
+// import { useState } from 'react';
 
 function Loadquiz({ children }) {
-  // const [isLoading, setIsloading ] = useState(false)
   const navigate = useNavigate();
   //   return navigate("/login");
   return <div>{children}</div>;
