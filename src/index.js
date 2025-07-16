dd283df5f8c03cc06923709196c038bc5093e1fa
Tony Izuogu <anthoizu@yahoo.com>
2025-07-15 21:59:02 -0500

    Setup Store Persist

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import  store  from './store/app/store';
+import  {store, persistor}  from './store/app/store';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import  Login  from './components/Pages/Login/Login'
 import Login from './pages/Login/Login';
 import { FirebaseApp } from './utils/firebase';
+import { PersistGate } from 'redux-persist/integration/react';
 // import Contact from './pages/Contact/Contact';
 // import HowtoWin from './pages/How/How';
 // import Frame from './pages/QuizForm/Quiz';
@@ -21,11 +22,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store} >
-
+      <PersistGate persistor={persistor}>
     {/* <BrowserRouter> */}
             <App />
     {/* </BrowserRouter> */}
-
+    </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/app/store.js
+++ src/store/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import quizReducer from '../../features/iframeSrc/quizUrlSlice';
 import authReducer from '../../features/iframeSrc/auth-slice';
 // import quizUrlSlice from "../../features/iframeSrc/quizUrlSlice";
@@ -8,11 +8,32 @@ import authReducer from '../../features/iframeSrc/auth-slice';
 import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
 
+// STORE SETUP WITHOUT PERSIST
+
+// export default configureStore({
+//     reducer: {
+//         quiz: quizReducer,
+//         authSlice: authReducer,
+//     },
+// })
 
 
-export default configureStore({
-    reducer: {
-        quiz: quizReducer,
-        authSlice: authReducer,
-    },
-})
+// Combined Reducer Solution ///
+const rootReducer = combineReducers({
+    quiz: quizReducer,
+    authSlice: authReducer
+
+});
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistReducers = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({reducer: persistReducers});
+
+const persistor = persistStore(store);
+
+export {store, persistor};
